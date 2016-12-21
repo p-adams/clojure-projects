cc238b48544909d39513dcdb46df1f40b1932949
Paul Adams <Paul Adams>
2016-12-20 19:20:48 -0500

    updated core.clj

--- calculator/src/calculator/core.clj
+++ calculator/src/calculator/core.clj
@@ -1,7 +1,27 @@
 (ns calculator.core
-  (:gen-class))
+  (:import (java.util Scanner)))
+
+(def scan (Scanner. *in*))
+
+
+
+(defn get-op []
+  (println "Enter first val")
+  (let [val (.nextInt scan)]
+    val))
+
+(defn operation-menu []
+  (println "choose from the options below: ")
+  (println "a) + ")
+  (println "b) - ")
+  (println "c) * ")
+  (println "d) / ")
+  (println "e) exit"))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn reload []
+  (use 'calculator.core :reload))
